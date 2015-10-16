aba081db3a24b00c8eae0dd197770778d15b9320
Connor McBride <cmm863@mst.edu>
2015-10-16 18:44:24 -0500

    Added assignment 3 file with imports.

---
+++ 3.py
@@ -0,0 +1,7 @@
+# Written by Connor McBride
+# Evolutionary Algorithms Assignment 1c
+# Self Adaptive EAs with Restarts
+
+import sys    # For command line arguments
+import json   # For config file parsing
+import random # For Process
