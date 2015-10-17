10abca726080e42a0da908c2634eea36f70676ad
Connor McBride <cmm863@mst.edu>
2015-10-16 19:29:57 -0500

    Working fitness and added sat.py

--- 3.py
+++ 3.py
@@ -2,9 +2,16 @@
 # Evolutionary Algorithms Assignment 1c
 # Self Adaptive EAs with Restarts
 
+# Library
 import sys    # For command line arguments
 import json   # For config file parsing
-import random # For Process
-import cnf
 
-test = cnf.CNF(['3'], 2, 1)
+# Local
+from cnf import CNF   
+from sat import SAT
+
+test_cnf = CNF("cnfs/1.cnf")
+test_sat = SAT(test_cnf.num_variables)
+print(test_sat.variables)
+test_sat.setFitness(test_cnf)
+print(test_sat.fitness)
--- cnf.py
+++ cnf.py
@@ -1,7 +1,17 @@
 class CNF:
-  clauses = []
+  clauses = list()
 
-  def __init__(self, clauses, num_clauses, num_variables):
-    self.clauses = clauses
-    self.num_clauses = num_clauses
-    self.num_variables = num_variables
+  def __init__(self, cnf_file):
+    with open(cnf_file) as f:
+      cnf_content = f.readlines()
+
+    for line in cnf_content:
+      if(line[0] == 'c'):
+        continue
+      if(line[0] == 'p'):
+        problem_arguments = line.rstrip().split(" ")
+        self.problem_type = problem_arguments[1]
+        self.num_variables = int(problem_arguments[2])
+        self.num_clauses = problem_arguments[3]
+        continue
+      self.clauses.append(line.rstrip().split(" "))
